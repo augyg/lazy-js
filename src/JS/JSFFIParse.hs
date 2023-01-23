7e17d55fb25ff81d4355b207077700d973e36fcf
lazyLambda <galen.sprout@gmail.com>
2023-01-23 10:33:03 +0000

    Added JS operations

--- src/JS/JSFFIParse.hs
+++ src/JS/JSFFIParse.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ForeignFunctionInterface, JavaScriptFFI #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TupleSections #-} 
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ScopedTypeVariables #-} 
 
 {-|
 Known Exceptions:
@@ -107,21 +108,6 @@ jsValue =
 -- | Arg is just meant to represent names for special scopes, not args passed in a statement
 data ArgName a = ArgName Name | ArgDef Name (JSValue a)
 data Function a = Function (Maybe Name) [ArgName a] [Dependency] [Statement]
--- jsFunction :: (Fractional a, Stream s m Char) => ParsecT s u m (Function a)
--- jsFunction = do
---   (fnName, args) <- functionHead
---   (bodyStatements, deps) <- functionBody args
---   pure $ Function (Just fnName) args deps bodyStatements
-  
---   where
---     functionBody headArgs = do
---       between' (char '{') (char '}') $ many (jsStatement headArgs) 
-    
---     functionHead = do
---       string "function"
---       fnName <- jsValidName
---       args <- jsArgTuple
---       pure (fnName, args) 
       
 
 jsArgTuple :: (Fractional a, Read a, Stream s m Char) => ParsecT s u m [ArgName a]
@@ -145,14 +131,10 @@ data JSStatement a = JSStatement [Dependency] (Maybe Name) (Expr a) --JS
 between1 :: Stream s m Char => ParsecT s u m open -> ParsecT s u m end -> ParsecT s u m a -> ParsecT s u m a 
 between1 open end match = open *> match <* end
 
-jsStatement :: (Fractional a, Read a, Stream s m Char) => a -> ParsecT s u m (JSStatement a)
-jsStatement _ = do
-  mName <- optionMaybe jsVarName
-  (expr, deps) <- jsExpression
-  -- | either followed by ; or \n
-  -- | oneOf ['\n', ';']
-  pure $ JSStatement deps mName expr
 
+
+
+  
 
 parseCallsWith :: (Fractional a, Read a, Stream s m Char) => ParsecT s u m (Name, [(Expr a, [Dependency])])
 parseCallsWith = do
@@ -173,38 +155,184 @@ data Ref' a = Prop Name | FnCall Name [(Expr a, [Dependency])] --(Function a)
 
 type RefChain a = [Ref' a] -- f(1).x(2).y AND `name` 
 
-data Expr a = Val (JSValue a) | Reference [Ref' a] --(RefChain a)
+--data Expr a = Val (JSValue a) | Reference [Ref' a] --(RefChain a)
 
 
 -- as this recurses, we will need to add dependencies
 -- | Actually this is only named expressions: for and while cannot be in this named context
 jsExpression :: (Fractional a, Read a, Stream s m Char) => ParsecT s u m ((Expr a), [Dependency])
-jsExpression = --do
-  
-  (,dependencyToDO) <$> (Val <$> jsValue)
-  <|> (,dependencyToDO) <$> existingConstructRef
-  -- <|> functionApplication 
-
-  -- <|> jsValidName -- IS REF
-  -- <|> loop 
-  -- | COMPLICATION:
-    -- WE CAN ALWAYS JUST DO .something so this is like a chain on the returned props which is
-    -- gonna be another `parseCallsWith` 
-
-  where
-    dependencyToDO :: [Dependency]
-    dependencyToDO = undefined
+jsExpression = do
+  (expr,deps) <- whnfValue
+  many (char ' ')
+  mComp <- (Nothing <$ oneOf ['\n', ';']) <|> (Just <$> do
+                                                  combinator <- someOperator
+                                                  many (char ' ') 
+                                                  exprANDdep <- jsExpression
+                                                  pure (combinator, exprANDdep)
+                                              )
+  case mComp of
+    Nothing -> pure (expr, deps)
+    Just (combinator, (expr2,deps2)) -> pure (Op combinator expr expr2, deps <> deps2)
     
-    existingConstructRef :: (Fractional a, Read a, Stream s m Char) => ParsecT s u m (Expr a)
-    existingConstructRef = Reference <$> (sepBy (name <|> nameWithArgs) (char '.'))
-      where
-        name = Prop <$> jsValidName 
-        nameWithArgs = do
-          (n, argExprWithDeps) <- parseCallsWith
-          pure $ FnCall n argExprWithDeps
+  where
+    whnfValue = (,[]) <$> (Val <$> jsValue)
+                <|> existingConstructRef
 
+    
+    existingConstructRef :: (Fractional a, Read a, Stream s m Char) => ParsecT s u m ((Expr a), [Dependency])
+    existingConstructRef = do -- Reference <$> (sepBy (name <|> nameWithArgs) (char '.'))
+      (refs, deps) <- unzip <$> (sepBy (name <|> nameWithArgs) (char '.'))
+      pure (Reference $ refs, mconcat deps)
+        where
+          name = do
+            n <- jsValidName
+            pure (Prop n, [n]) 
+          nameWithArgs = do
+            (n, argExprWithDeps) <- parseCallsWith
+            pure $ (FnCall n argExprWithDeps, mconcat $ fmap snd argExprWithDeps) 
 
+someOperator :: Stream s m Char => ParsecT s u m Operator
+someOperator =
+  (Multiply <$ (string "*"))
+  <|> (Plus <$ (string "+"))
+  <|> (Subtract <$ (string "-"))
+  <|> (Exponentiation <$ (string "**"))
+  <|> (Division <$ (string "/"))
+  <|> (Modulo <$ (string "%"))
+  <|> (Equal <$ (string "=="))
+  <|> (EqualSameType <$ (string "==="))
+  <|> (NotEqual <$ (string "!="))
+  <|> (NotEqualOrNotSameType <$ (string "!=="))
+  <|> (GreaterThan <$ (string ">"))
+  <|> (LessThan <$ (string "<"))
+  <|> (LessOrEqual <$ (string "<="))
+  <|> (GreaterOrEqual <$ (string ">="))
+  <|> (Ternary <$ (string "?"))
+  <|> (AND <$ (string "&&"))
+  <|> (OR <$ (string "||"))
+  <|> (NOT <$ (string "!")) -- Should i make the second Expr a Maybe Expr? I may have to
+  <|> (ANDB <$ (string "&"))
+  <|> (ORB <$ (string "|"))
+  <|> (NOTB <$ (string "~"))
+  <|> (XORB <$ (string "^"))
+  <|> (Shift <$> shift)
+  where
+    shift = (try $ string ">>") <|> (try $ string "<<") <|> (try $ string ">>>")
+  
+  
       
+
+
+-- jsOperation = do
+--   jsExpression
+--   many (char ' ') 
+--   (oneOf ['\n', ';']) <|> do 
+--     combinator <- someOperator
+--     someValueWHNF <- jsOperation
+    
+
+type Expr' a = (Expr a, [Dependency])
+--data [] a = [] | a : []
+--data Op = Op Operator Expr' Expr'
+-- TODO(galen): combine with Expr so that this can be a recursively defined type
+  -- OpConstructor Operator Op Op 
+
+
+data Expr a = Val (JSValue a) | Reference [Ref' a] | Op Operator (Expr a) (Expr a) --(RefChain a)
+
+
+
+-- Note that these can literally pair with any two JS Values 
+data Operator = Multiply
+              | Plus
+              | Subtract
+              | Exponentiation
+              | Division
+              | Modulo
+              | Equal -- (==)
+              | EqualSameType  -- (===)
+              | NotEqual
+              | NotEqualOrNotSameType
+              | GreaterThan
+              | LessThan
+              | LessOrEqual
+              | GreaterOrEqual
+              | Ternary -- ? ~~ isTrue
+              | AND
+              | OR
+              | NOT
+              | ANDB
+              | ORB
+              | NOTB
+              | XORB
+              | Shift String 
+
+-- Likely should run in the case of no naming
+-- | Should call this jsOperation
+jsStatement :: (Fractional a, Read a, Stream s m Char) => a -> ParsecT s u m (JSStatement a)
+jsStatement _ = do
+  mName <- optionMaybe jsVarName -- todo: assignment operators -- TODO multiple vars per var keyword
+  (expr, deps) <- jsExpression
+  -- | either followed by ; or \n
+  -- | oneOf ['\n', ';']
+  pure $ JSStatement deps mName expr
+
+jsOperation = jsStatement
+
+jsStatement' =
+  jsOperation
+  <|> whileLoop
+  <|> forLoop
+  <|> jsFunction
+  <|> objectDeclaration 
+
+forLoop = undefined
+objectDeclaration = undefined
+  -- basically just properties where you can have functions
+
+-- NOTE: Need to make a concept like jsStatement except it 
+
+data While = While Condition [Dependency] [JSStatement] 
+whileLoop = do
+  normal <|> doWhile
+  where
+
+    normal = do
+      string "while"
+      headExpr <- between1 (char '(') (char ')') jsExpression
+      statements <- between' (char '{') (char '}') jsStatement
+      pure $ While headExpr _ statements
+      
+
+    doWhile = do
+      string "do"
+      statements <- between' (char '{') (char '}') jsStatement
+      optional $ char '\n'
+      string "while"
+      many (char ' ')
+      headExpr <- between1 (char '(') (char ')') jsExpression
+      pure $ While headExpr (getFromStatements statements) statements
+
+
+jsFunction :: (Fractional a, Stream s m Char) => ParsecT s u m (Function a)
+jsFunction = do
+  (fnName, args) <- functionHead
+  (bodyStatements, deps) <- functionBody args
+  pure $ Function (Just fnName) args deps bodyStatements
+  
+  where
+    functionBody headArgs = do
+      between' (char '{') (char '}') $ (jsStatement headArgs <|> returnStatement)
+
+      where returnStatement = undefined
+    
+    functionHead = do
+      string "function"
+      fnName <- jsValidName
+      args <- jsArgTuple
+      pure (fnName, args) 
+
+
       
 -- | Note that is purely done out of weird timing
 -- | as long as I have the Expression as 
